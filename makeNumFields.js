371d86034834332e137bcb8a2365f36e8c4c4a98
botwcooking <42941340+botwcooking@users.noreply.github.com>
2018-09-03 17:22:28 +0200

    #3

--- makeNumFields.js
+++ makeNumFields.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 			$("#marker" + i).append("<p class=\"col-sm-6\">" + item.name.charAt(0).toUpperCase() + "</p>");
 			$("#marker" + i).append("<div class=\"col-sm-6\"></div>");
 		}
-		$("#inputs" + column).append("<div id=\"input" + i + "\">");
+		$("#inputs" + column).append("<div class=\"row\" id=\"input" + i + "\">");
 		$("#input" + i).append("<p class=\"col-sm-6\">" + item.name + "</p>");
 		$("#input" + i).append("<input class=\"col-sm-6\" type=\"number\" min=\"0\" value=\"2\" id=\"inputItem" + item.id + "\">")
 		//$("#inputs" + column).children().eq(0).append(item.name + "<br />");
